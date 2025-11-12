34d564a945ae9ebb0b4d6e69f23a2de43d305cd5
Jiwon Kim <115946088+JiwonKim-kr@users.noreply.github.com>
2025-11-12 14:15:30 +0900

    error test
    Signed-off-by: Jiwon Kim <115946088+JiwonKim-kr@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
 public class Main {
     // update
-    public static void main(String[] args) {
-        GameController controller = new GameController();
-        controller.start();
-    }
 }
---
+++ src/Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        GameController controller = new GameController();
+        controller.start();
+    }
+}
